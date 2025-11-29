4f9d88a6574bd53cad278a45a157b92b227ea822
Justine Chollo Catanghal <chollocatanghal@gmail.com>
2025-11-29 17:15:20 +0800

    Add files via upload

---
+++ PRLM_Assign/include/footer.php
@@ -0,0 +1,7 @@
+<!--Catanghal, Justine Chollo C | CYB 201-->
+
+<div class="footer">
+            <p><strong>Gotta Catch 'Em All!</strong></p>
+            <p>&copy;2025 PokéMart PH. All Rights Reserved.</p>
+            <p>Store Owner: <a href="#">Justine Chollo Catanghal</a> | CYB 201</p>
+</div>
